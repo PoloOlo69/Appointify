2be234a40bd9b5b74b8bb0bfd9b35a1535d014e2
Paul <104866501+PoloOlo69@users.noreply.github.com>
2022-09-30 16:22:38 +0200

    -

--- src/Schedule.java
+++ src/Schedule.java
@@ -5,60 +5,61 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Schedule implements APO {
-    static Pattern locationPattern = Pattern.compile("(Bierstadt|Rheinstr\\.|Dürerplatz|Erbenheim)");
-    static Pattern contentPattern = Pattern.compile("(Bierstadt|Rheinstr\\.|Dürerplatz|Erbenheim|Container)\\s([ZäöüÄÖÜßA-Za-z]*[-\\d]?[ZäöüÄÖÜßA-Za-z]*)\\s?(\\d\\d:\\d\\d)?");
+	static Pattern locationPattern = Pattern.compile("(Bierstadt|Rheinstr\\.|Dürerplatz|Erbenheim)");
+	static Pattern contentPattern = Pattern.compile("(Bierstadt|Rheinstr\\.|Dürerplatz|Erbenheim|Container)\\s([ZäöüÄÖÜßA-Za-z]*[-\\d]?[ZäöüÄÖÜßA-Za-z]*)\\s?(\\d\\d:\\d\\d)?");
 
-    public static @org.jetbrains.annotations.NotNull List<APO.Appointment> schedule(Weekday weekday, String raw) throws Exception{
-        String date = getDate(raw);
+	public static @org.jetbrains.annotations.NotNull List<APO.Appointment> schedule(Weekday weekday, String raw) throws Exception{
+		String date = getDate(raw);
 
-        List<APO.Appointment> appointments = new LinkedList<>();
-        Matcher content = contentPattern.matcher(raw);
-        Matcher location = locationPattern.matcher(raw);
-        while(content.find())
-        {
-            LocalTime t1, t2;
-            String place, starts, ends;
-            String name = content.group(2);
-            switch (content.group(1))
-            {
-                case "Container" ->
-                {
-                    location.find();
-                    place = location.group();
-                    t1 = catalogue.get(place).get(weekday).opens();
-                    t2 = catalogue.get(place).get(weekday).handover();
-                }
+		List<APO.Appointment> appointments = new LinkedList<>();
+		Matcher content = contentPattern.matcher(raw);
+		Matcher location = locationPattern.matcher(raw);
+		while(content.find())
+		{
+			LocalTime t1, t2;
+			String place, starts, ends;
+			String name = content.group(2);
+			switch(content.group(1))
+			{
+				case "Container" ->
+				{
+					location.find();
+					place = location.group();
+					t1 = catalogue.get(place).get(weekday).opens();
+					t2 = catalogue.get(place).get(weekday).handover();
+				}
 
-                case "Bierstadt", "Dürerplatz", "Rheinstr.", "Erbenheim" ->
-                {
-                    place = content.group(1);
-                    t1 = catalogue.get(place).get(weekday).handover();
-                    t2 = catalogue.get(place).get(weekday).closes();
-                }
+				case "Bierstadt", "Dürerplatz", "Rheinstr.", "Erbenheim" ->
+				{
+					place = content.group(1);
+					t1 = catalogue.get(place).get(weekday).handover();
+					t2 = catalogue.get(place).get(weekday).closes();
+				}
 
-                default ->
-                {
-                    place = location.group();
-                    t1 = catalogue.get(place).get(weekday).handover();
-                    t2 = catalogue.get(place).get(weekday).closes();
-                }
-            }
-            starts = getDate.apply(date.concat(String.valueOf(t1)));
-            ends = getDate.apply(date.concat(String.valueOf(t2)));
-            appointments.add(new Appointment(name, "Arbeiten ".concat(place), starts, ends, dictionary.get(place)));
-        }
-        return appointments;
-    }
+				default ->
+				{
+					place = location.group();
+					t1 = catalogue.get(place).get(weekday).handover();
+					t2 = catalogue.get(place).get(weekday).closes();
+				}
+			}
+			starts = getDate.apply(date.concat(String.valueOf(t1)));
+			ends = getDate.apply(date.concat(String.valueOf(t2)));
+			appointments.add(new Appointment(name, "Arbeiten ".concat(place), starts, ends, dictionary.get(place)));
+		}
+		return appointments;
+	}
 
-    private static @org.jetbrains.annotations.NotNull String getDate(String raw) throws Exception{
-        Matcher dateM = Pattern.compile("(\\d\\d\\.\\d\\d\\.)").matcher(raw);
-        if(dateM.find())
-        {
-            return dateM.group().concat("2022");
-        }
-        else
-        {
-            throw new Exception("NO DATE PROVIDED");
-        }
-    }
+	private static @org.jetbrains.annotations.NotNull String getDate(String raw) throws Exception{
+		Matcher dateM = Pattern.compile("(\\d\\d\\.\\d\\d\\.)").matcher(raw);
+		if(dateM.find())
+		{
+			return dateM.group().concat("2022");
+		}
+		else
+		{
+			throw new Exception("NO DATE PROVIDED");
+		}
+	}
+
 }
--- src/SchedulePreProcessor.java
+++ src/SchedulePreProcessor.java
@@ -2,19 +2,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.function.Predicate;
-
+import java.util.stream.Collectors;
 
 public class SchedulePreProcessor {
-    final static String DAYS = "(\\s(\\w*tag|Mittwoch))\\s";// Nur an tagen die mit g aufhören und Mittwoch.
-    final static Predicate<String> UNWANTED = Predicate.not(s -> s.matches("([\\d\\d:\\d\\d\\s]{7,})|(.*Arbeitsplan.*)") || s.isBlank());
+    final static String DAYS = "\\s(\\w*tag|Mittwoch)\\s";// Nur an tagen die mit g aufhören und Mittwoch.
+    final static Predicate<String> UNWANTED = Predicate.not(s -> s.isBlank() || s.matches("([\\d\\d:\\d\\d\\s?]{7,})|(.*Arbeitsplan.*)"));
 
     public static String[] readClean(String src) throws IOException{
         StringBuffer sb = new StringBuffer();
-        Files.newBufferedReader(Path.of(src)).lines()
-                .filter(UNWANTED)
-                .forEach(s -> {
-                    sb.append(s).append(System.lineSeparator());
-                });
-        return sb.toString().split(DAYS);
+        return Files.newBufferedReader(Path.of(src)).lines()
+                    .filter(UNWANTED)
+                    .map(s -> s.concat(System.lineSeparator()))
+                    .collect(Collectors.joining())
+                    .split(DAYS);
     }
+
 }
