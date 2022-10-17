4b6ce65977db7e86dbda13869753c533d29294e1
polo <paul.duerrwang@student.hs-rm.de>
2022-10-17 11:40:09 +0200

    - fixed and cleaned: PreProcessor.java - TODO: Schedule.java major cleanup, improve RegEx

--- src/main/java/APO.java
+++ src/main/java/APO.java
@@ -9,10 +9,46 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 public interface APO {
-    ZoneId ZONE = ZoneId.of("Europe/Berlin");
+
+
+    enum Weekday {
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+    }
+    enum Location {
+        BIERSTADT("Testzentrum Bierstadt: Rostocker Str. 5, 65191 Wiesbaden Bierstadt"),
+        RHEINSTRASSE("Testzentrum Kreuz Apotheke: Oranienstr. 1, 65185 Wiesbaden"),
+        ERBENHEIM("Testzentrum Erbenheim: Berliner Str. 277, 65205 Wiesbaden Erbenheim"),
+        DÜRERPLATZ("Testzentrum Duerer Apotheke: Albrecht-Dürer-Straße 1, 65195 Wiesbaden");
+        final String adressOf;
+        Location(String address){
+            this.adressOf = address;
+        }
+    }
+
+
+    record BusinessHour(LocalTime opens, LocalTime closes, LocalTime handover) {}
+    record Appointment(String who, String title, String starts, String ends, Location where) {
+
+        String toICS() {
+            return ("BEGIN:VEVENT\r\n" +
+                    "DTSTAMP:%s\r\n" +
+                    "SUMMARY:%s\r\n" +
+                    "UID:%s\r\n" +
+                    "DTSTART:%s\r\n" +
+                    "DTEND:%s\r\n" +
+                    "LOCATION:%s\r\n" +
+                    "END:VEVENT\r\n")
+                    .formatted(STAMP.get(), title, who + "_" + UUID.randomUUID(), starts, ends, where.adressOf);
+        }
+    }
+
+
     DateTimeFormatter DTFR = DateTimeFormatter.ofPattern("uuuuMMdd'T'HHmmss'Z'");
+    Supplier<String> STAMP = () -> LocalDateTime.ofInstant(Instant.now(), ZoneId.of("Z")).format(DTFR);
     Function<String, String> getDate = s -> LocalDateTime.parse(s, DateTimeFormatter.ofPattern("dd.MM.yyyyHH:mm")).minusHours(2).format(APO.DTFR);
     BiFunction<Integer, Integer, LocalTime> time = LocalTime::of;
+
+
     Map<Weekday, BusinessHour> bierstadt = Map.of
             (
                     Weekday.MONDAY, new BusinessHour(time.apply(7, 0), time.apply(18, 0), time.apply(12, 55)),
@@ -53,49 +89,8 @@ public interface APO {
                     Weekday.SATURDAY, new BusinessHour(time.apply(8, 50), time.apply(16, 0), time.apply(13, 25)),
                     Weekday.SUNDAY, new BusinessHour(time.apply(8, 45), time.apply(14, 0), time.apply(11, 30))
             );
-    Map<String, Map<Weekday, BusinessHour>> catalogue = Map.of("Rheinstr.", rheinstrasse, "Bierstadt", bierstadt, "Erbenheim", erbenheim, "Dürerplatz", dürerplatz);
-    Map<String, Location> dictionary = Map.of("Bierstadt", Location.BIERSTADT, "Rheinstr.", Location.RHEINSTRASSE, "Dürerplatz", Location.DÜRERPLATZ, "Erbenheim", Location.ERBENHEIM);
-    Supplier<String> STAMP = () -> LocalDateTime.ofInstant(Instant.now(), ZoneId.of("Z")).format(DTFR);
-    Supplier<String> UID = () -> Instant.now().toString().concat(System.getProperty("user.name"));
-
-    enum Location {
-        BIERSTADT("Testzentrum Bierstadt: Rostocker Str. 5, 65191 Wiesbaden Bierstadt"),
-        RHEINSTRASSE("Testzentrum Kreuz Apotheke: Oranienstr. 1, 65185 Wiesbaden"),
-        ERBENHEIM("Testzentrum Erbenheim: Berliner Str. 277, 65205 Wiesbaden Erbenheim"),
-        DÜRERPLATZ("Testzentrum Duerer Apotheke: Albrecht-Dürer-Straße 1, 65195 Wiesbaden");
-        final String adressOf;
-
-        Location(String address) {
-            this.adressOf = address;
-        }
-    }
-
-    enum Weekday {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY}
-
-    record BusinessHour(LocalTime opens, LocalTime closes, LocalTime handover) {
-    }
-
-    record Appointment(String who, String title, String starts, String ends, Location where) {
-        public Appointment(String who, String title, String starts, String ends, Location where) {
-            this.who = who;
-            this.title = title;
-            this.starts = starts;
-            this.ends = ends;
-            this.where = where;
-        }
-
-        String toICS() {
-            return ("BEGIN:VEVENT\r\n" +
-                    "DTSTAMP:%s\r\n" +
-                    "SUMMARY:%s\r\n" +
-                    "UID:%s\r\n" +
-                    "DTSTART:%s\r\n" +
-                    "DTEND:%s\r\n" +
-                    "LOCATION:%s\r\n" +
-                    "END:VEVENT\r\n")
-                    .formatted(STAMP.get(), title, who + "_" + UUID.randomUUID(), starts, ends, where.adressOf);
-        }
-
-    }
-
+    Map<String, Location> dictionary =
+            Map.of("Bierstadt", Location.BIERSTADT, "Rheinstr.", Location.RHEINSTRASSE, "Dürerplatz", Location.DÜRERPLATZ, "Erbenheim", Location.ERBENHEIM);
+    Map<String, Map<Weekday, BusinessHour>> catalogue =
+            Map.of("Rheinstr.", rheinstrasse, "Bierstadt", bierstadt, "Erbenheim", erbenheim, "Dürerplatz", dürerplatz);
 }
